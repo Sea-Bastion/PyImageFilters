f127f4aa514156d5a9838d3b3b8ddfb744e03c20
Sebastian Cypert <sebastian.cypert@gmail.com>
2023-11-19 22:39:10 -0600

    Finished Kuwahara, Probably done for awhile

--- Kuwahara/Kuwahara.py
+++ Kuwahara/Kuwahara.py
@@ -16,59 +16,81 @@ from scipy import ndimage
 import argparse
 import json
 from matplotlib import pyplot as plt
+from multiprocessing import Pool
+from functools import partial
 
 """
 I'm just going to hard code the values for now but eventually I want to make them args
 """
 
 N = 8
-windowRadius = 25
 MaskPath  = 'resources/Mask.json'
 ImgInPath = 'resources/monkey.jpg'
 
 # ----------------------- Import Masks -------------------
+#%% Import Masks
 
 with open(MaskPath, 'r') as MaskFile:
     Masks = json.load(MaskFile)
     
 Masks = [np.array(i) for i in Masks]
+WindowSize = Masks[0].shape[0]
+del MaskFile
 
 
 # ----------------------- Import Input Image ----------------------
-
+#%% Import Img
 
 with Image.open(ImgInPath) as rawImage:
     Img = np.array( rawImage ).transpose(1,0,2).astype('float32')
 
-Img /= 255
+#Img /= 255
 dims = Img.shape
+del rawImage
 
+# ---------------------- directional StDev & Mean -------------------------
+#%% Mean and StDev Gen
 
-# ---------------------- directional StDev -------------------------
-
-Means = np.zeros( (N, dims[0], dims[1], 3) )
-StDev = np.zeros( (N, dims[0], dims[1]) )
-
-          
-for n in range(N):
-    FullMask = np.stack( [np.zeros((51,51)), Masks[n], np.zeros((51,51))] ,2)
+def ProcessDirection(n, image):
+    global Means, StDev
     
-    Mean = ndimage.correlate(Img, FullMask, mode='wrap')
-    SqMean = ndimage.correlate(Img*Img, FullMask, mode='wrap')
+    FullMask = np.stack( [np.zeros(Masks[n].shape), Masks[n], np.zeros(Masks[n].shape)] ,2)
     
-    Means[n, :, :] = Mean
-    StDev[n, :, :] = np.sqrt( np.mean(SqMean - (Mean*Mean), 2) ) #mean go to euc dist
-    #because this is pre sqrt you may just be able to sum before sqrt to get dist
+    Mean = ndimage.correlate(image, FullMask, mode='mirror')
+    SqMean = ndimage.correlate(image*image, FullMask, mode='mirror')
+    
+    Varriance = np.sum( SqMean - (Mean*Mean), 2 ) #Euclidian Distance, trust me
+    Varriance[Varriance < 0] = 0
     
     
     print('finished n: {:n}'.format(n))
+    return Mean, np.sqrt(Varriance)
     
-
+with Pool(8) as p:
+    f = partial( ProcessDirection, image=Img )
+    PoolOut = p.map(f, np.arange(8))
+    
+Means = np.array([ x[0] for x in PoolOut ])
+StDev = np.array([ x[1] for x in PoolOut ])
+    
 # ---------------------- Final Image Gen-------------------------------
 #%% Final Image Gen
 
 q=8
-alpha = 1 / (1 + (StDev**q) )
+alpha = 1 / ( 1 + (StDev**q) )
+alpha = np.stack(3*[alpha], axis=3)
 alphaSum = np.sum(alpha, 0)
 
 outputImg = np.sum(alpha * Means, 0)/alphaSum
+
+# ----------------------- Display Image -----------------------------
+#%% Show Img
+
+plt.imshow( outputImg.transpose(1,0,2)/255 )
+plt.show()
+
+
+#%% Save Img
+
+SaveImg = outputImg.transpose(1,0,2)
+Image.fromarray(np.uint8(SaveImg)).save("KuwaharaOut.jpg")
